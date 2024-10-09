cf569908b54b49d2a5c0f23dd086b186a6a1a734
MAYUR RATHORE <hackercodder@gmail.com>
2024-10-09 15:00:07 +0530

    ok

---
+++ n.js
@@ -0,0 +1,65 @@
+ 
+ 
+// //   f1()
+// //   {
+// //     let x=20;
+// //  }
+ 
+ 
+ 
+// //  console.log(x)
+// //  var x=80//tempraral dead zone
+
+
+
+//  console.log("seggsg"+"egeeegewef")
+//  console.log("hii","efgyegwfy")
+//   console.log('${'df'}')
+// let x=23;
+
+
+
+
+
+
+
+//  x=> x*2;
+// console.log((2));
+
+
+
+// (a,b)=>{
+// this.a=null;
+// this.b= null;
+//     return  a;
+//  } 
+
+//  console.log((2))
+ 
+
+
+
+
+//         ARROW FUNCTION
+
+let f1=(a,b)=>{
+    a+b;
+    console.log("hello")
+return a+b;
+}
+
+console.log(f1(2,3));
+
+
+//                 ES6
+// 1)  CODE KA SCOPE BADH GAYA H ES6 KE BAAD.
+// 2)  ARROW FUNCTION ES6 ME AASA 
+
+// 3)  TEMPLATE  LITERAL  ES6 ME AAYA.
+// 4)  DEFAULT DENE KA FEATURE ES6 ME START HUA.
+    
+
+
+//                  ES5
+// 1) OBJECT AND PROTOTYPE ARE FOLLOW
+
---
+++ vvv.js
@@ -0,0 +1,13 @@
+
+var a = undefined; 
+var b = null; 
+// var linebreak = "<br />"; 
+ 
+// document.write ("((a > b) ? 100 : 200) => "); 
+// result = (a > b) ? 100 : 200; 
+// document.write(result); 
+// document.write(linebreak); 
+ 
+ document.write (a<b?1000:2000); 
+
+document.write(typeof a)
